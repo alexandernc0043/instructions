e3a4a853575bd0304ebceea56b079f0387827d7c
Alexander Prechtel <aprechtel0043@gmail.com>
2025-09-09 10:17:24 -0400

    Added warning label

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,17 @@ import "@/styles/globals.css";
 import Footer from "@/components/layout/Footer";
 import AutoBreadcrumbs from "@/components/navigation/AutoBreadcrumbs";
 import {HeaderProvider} from "@/components/layout/HeaderProvider";
+import Warning from "@/components/feedback/Warning";
+
 export default function RootLayout({children}: Readonly<{ children: ReactNode; }>) {
 
     return (
         <html lang="en">
         <body className="min-h-screen antialiased">
         <div className="mx-auto w-full max-w-3xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl px-4 sm:px-6 py-6 sm:py-8">
-            <AutoBreadcrumbs />
+            <Warning
+                msg={"THIS WEBSITE IS A WORK IN PROGRESS PLEASE DO NOT SOLELY RELY ON THIS WEBSITE FOR ASSIGNMENT INSTRUCTIONS. PLEASE USE THE OFFICIAL SOURCE (CANVAS)"}/>
+            <AutoBreadcrumbs/>
             <HeaderProvider/>
             {children}
             <Footer/>
